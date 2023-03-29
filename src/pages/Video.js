17b2381e7425d91f4e05a583260e13a3d47eeadb
RaulPereira1 <raul.pereira1@outlook.com>
2023-03-29 20:46:40 -0300

    primeiro commit

---
+++ src/pages/Video.js
@@ -0,0 +1,17 @@
+import "./Video.css";
+
+function Video() {
+    return (
+        <div className="Video">
+            
+            <video
+            className="video__player"
+            src="https://firebasestorage.googleapis.com/v0/b/jornadadevtr.appspot.com/o/WhatsApp%20Video%202023-03-29%20at%2020.04.52.mp4?alt=media&token=db4be029-27cc-4af9-b75d-3ce5fd1b1fe2"
+            >
+                
+            </video>
+        </div>
+        )
+}
+
+export default Video
