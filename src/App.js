399b986810479825636cb175bd6fd8b61f6387f9
raulpereira1 <raul.pereira1@outlook.com>
2023-04-02 20:53:24 -0300

    adicionando css para mobile

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import React, { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore/lite";
 
 function App() {
+
+let maxHeight;
+if(window.innerHeight <=800){
+  maxHeight = window.innerHeight
+}
+
+
   const [video, setVideos] = useState([]);
 
   async function getVideos() {
@@ -18,7 +25,7 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
+    <div className="App"  style={{maxHeight: maxHeight + "px"}}>
       <div className="app__videos">
         {video.map((item) => {
           return (
